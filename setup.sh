15d05a878b9a8b116d6295572ce6cb1b24cde562
Methan <methanroyal@gmail.com>
2022-06-30 09:59:52 +0530

    gitpod

---
+++ setup.sh
@@ -0,0 +1,30 @@
+sudo apt update && sudo apt upgrade -y
+sudo reboot command
+sudo apt --purge autoremove
+sudo apt install update-manager-core
+sudo do-release-upgrade
+lsb_release -a
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+sudo apt install yarn
+yarn --version
+./ui/build
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install default-jdk -y
+wget www.scala-lang.org/files/archive/scala-2.13.0.deb
+sudo dpkg -i scala*.deb
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+sudo apt-get update
+sudo apt-get install sbt
+sbt test
+echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
+echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+sudo apt-get update
+sudo apt-get install sbt
+sudo apt update
+sudo apt install redis-server
+redis-server
